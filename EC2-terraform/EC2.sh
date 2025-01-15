01b2fe77bd3e2864b3b51cbb007d1381588f3ede
gyenoch <www.gyenoch@gmail.com>
2025-01-15 10:59:15 +0000

    EC2-terraform/ updated

--- EC2-terraform/EC2.sh
+++ EC2-terraform/EC2.sh
@@ -29,12 +29,12 @@ docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 # Install Nexus 
 #docker run -d -p 8081:8081 --name nexus sonatype/nexus3
 
-# install trivy
-# sudo apt-get install wget apt-transport-https gnupg lsb-release -y
-# wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
-# echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
-# sudo apt-get update
-# sudo apt-get install trivy -y
+install trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy -y
 
 # Install AWS CLI 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
